36d92e962769b74a065889e80b40f98fa1e4109c
Vinicius Tadeu <tadeusrox@gmail.com>
2016-12-04 17:44:31 -0200

    Day 1 of 30 days to code

---
+++ 30_days_of_code/day1.rb
@@ -0,0 +1,12 @@
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+read_int = gets.to_i
+read_double = gets.to_f
+read_string = gets
+# Print the sum of both integer variables on a new line.
+puts i + read_int
+# Print the sum of the double variables on a new line.
+puts d + read_double
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts s + read_string
